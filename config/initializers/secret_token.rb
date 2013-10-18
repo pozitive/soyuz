67e2fe0b5aedbad9efe63d0c5cd12a12d5e0a2e0
Dmitry Sukhikh <china@pozitive.biz>
2013-10-18 10:10:32 +0700

    Start webapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Soyuz::Application.config.secret_key_base = '75156b9fb87ae05c1218527063e054b2257b6a89e572a28f57c3fc48b84129d83e3e5713788dd52602e114dfb1e133a3b203cb72bf42a20fca73bf926a5ed92a'
