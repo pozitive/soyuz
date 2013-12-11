966d45dacf28c263d115c6e3a06db92febd81112
Dmitry Sukhikh <china@pozitive.biz>
2013-12-11 21:47:48 +0700

    Add precompile true for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Soyuz::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # https://github.com/ejholmes/active_admin_editor#heroku
+  config.initialize_on_precompile = true
+
 end
